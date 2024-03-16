0cd4434788d0eb6f267bf05b147ca39f5cb968de
Md Ashraful Islam <ms785314@gmail.com>
2024-03-16 17:24:52 +0600

    remove showcase section from homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,14 +3,13 @@ import PopularBrands from "../PopularBrands/PopularBrands";
 import PopularCategories from "../PopularCategories/PopularCategories";
 import NewArrival from "../NewArrival/NewArrival";
 import OurCommitments from "../OurCommitments/OurCommitments";
-import ShowcaseInstruments from "./ShowcaseInstruments";
+
 
 const Home = () => {
   return (
     <div className="container mx-auto p-5">
       <Hero />
       <PopularBrands />
-      <ShowcaseInstruments />
       <PopularCategories />
       <NewArrival />
       <OurCommitments />
