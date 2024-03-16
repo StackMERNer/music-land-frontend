eadf94f256ac3e117e4d1c04825fab1897157d13
Md Ashraful Islam <ms785314@gmail.com>
2024-03-16 19:04:06 +0600

    Refactor  InstrumentDetails page

--- src/pages/Instruments/InstrumentDetails.tsx
+++ src/pages/Instruments/InstrumentDetails.tsx
@@ -1,20 +1,14 @@
-import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
 import useCart, { AddedProduct } from "../../hooks/useCart";
 import useInstrument from "../../hooks/useInstrument";
-import useOrders from "../../hooks/useOrders";
-import useUser from "../../hooks/useUser";
-import { auth } from "../../services/firebase";
 import Cart from "./Cart";
 import InstrumentDetailsCard from "./InstrumentDetailsCard";
 
 const InstrumentDetails = () => {
   const { id } = useParams();
   const { instrument } = useInstrument(id || "");
-  // const [cart, setCart] = useState<AddedProduct[]>([]);
   const { cart, setCart } = useCart();
-  const [gUser] = useAuthState(auth);
-  const { user } = useUser(gUser?.uid);
+
   const addToCard = () => {
     const selectedItem = cart.find((item) => item.id === instrument?._id);
     if (!selectedItem) {
@@ -30,7 +24,6 @@ const InstrumentDetails = () => {
       localStorage.setItem("cart", JSON.stringify([...cart, newItem]));
     }
   };
-  const { orders } = useOrders(user?._id ?? "");
   return (
     <div className="container mx-auto flex justify-center min-h-screen pt-5 pb-3 gap-4 items-baseline">
       {instrument && (
@@ -38,18 +31,7 @@ const InstrumentDetails = () => {
       )}
 
       <div className="self-start">
-        <div className="text-white">
-          {orders.map((order, i) => (
-            <div key={i}>
-              {order.items.map((item, i) => (
-                <div key={i}>
-                  <p>{item.name}</p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <Cart items={cart} />
+        <Cart />
       </div>
     </div>
   );
